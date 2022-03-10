2ac71a55e3f3bdd8e8c91bd578c1588c524dca85
kraktus <kraktus@users.noreply.github.com>
2022-03-10 22:50:21 +0100

    replace generic by macro implementation

--- src/map.rs
+++ src/map.rs
@@ -1,25 +1,32 @@
-use num_traits::Unsigned;
+use paste::paste;
 
+macro_rules! impl_for {
+	($($struct_name:ident, $u:ty,)+) => {$(
+paste! {
 #[derive(Clone, Debug)]
-pub struct MapOfIndexes<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> {
-    inner: Vec<T>,
+pub struct [<MapOfIndexes $struct_name>] <const KEY_NB_BITS: usize> {
+    inner: Vec<$u>,
 }
 
 #[derive(Clone, Debug)]
-pub struct SortedMapOfIndexes<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> {
-    inner: Vec<T>,
+pub struct [<SortedMapOfIndexes $struct_name>]<const KEY_NB_BITS: usize> {
+    inner: Vec<$u>,
 }
 
-impl<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> MapOfIndexes<T, KEY_NB_BITS> {
-    fn push(&mut self, element: T) {
+impl<const KEY_NB_BITS: usize> [<MapOfIndexes $struct_name>]<KEY_NB_BITS> {
+    pub fn new() -> Self {
+        Self {inner: Vec::new()}
+    }
+
+    fn push(&mut self, element: $u) {
         self.inner.push(element);
     }
 }
 
-impl<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> From<MapOfIndexes<T, KEY_NB_BITS>>
-    for SortedMapOfIndexes<T, KEY_NB_BITS>
+impl<const KEY_NB_BITS: usize> From<[<MapOfIndexes $struct_name>]<KEY_NB_BITS>>
+    for [<SortedMapOfIndexes $struct_name>]<KEY_NB_BITS>
 {
-    fn from(mut map_of_index: MapOfIndexes<T, KEY_NB_BITS>) -> Self {
+    fn from(mut map_of_index: [<MapOfIndexes $struct_name>]<KEY_NB_BITS>) -> Self {
         map_of_index.inner.sort();
         Self {
             inner: map_of_index.inner,
@@ -27,10 +34,24 @@ impl<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> From<MapOfIndexes<T,
     }
 }
 
-impl<T: Unsigned + std::cmp::Ord, const KEY_NB_BITS: usize> SortedMapOfIndexes<T, KEY_NB_BITS> {
-    fn push(&mut self, element: T) {
-    	if self.inner.last().map(|x| x >= element).unwrap_or(false) {
-    		panic!("Attempted to push a lower element {:?}, last element value is: ", element, self.inner.last());
+impl<const KEY_NB_BITS: usize> [<SortedMapOfIndexes $struct_name>]<KEY_NB_BITS> {
+    fn push(&mut self, element: $u) {
+    	if self.inner.last().map(|x| x >= &element).unwrap_or(false) {
+    		panic!("Attempted to push a lower element {:?}, last element value is: {:?}", element, self.inner.last());
     	}
+    }
+}
+}
+)+}}
+
+impl_for! {U8, u8, U16, u16, U32, u32, U64, u64, U128, u128, Usize, usize,}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn name() {
+        MapOfIndexesU8::<12>::new();
     }
 }
