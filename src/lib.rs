327f44e8b9d331eb95d543c6a3dc38049911e7ab
kraktus <kraktus@users.noreply.github.com>
2022-03-13 23:28:52 +0100

    WIP: generalise `KeyValue` so that key can either return `Self::K` or `&Self::K`

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,24 @@ use std::cmp::Ordering;
 use std::fmt::Debug;
 use thiserror::Error;
 
-pub trait KeyValue {
+pub trait KeyValue<'a> {
     type K: Ord + Debug;
     type V;
-    fn key(&self) -> &Self::K;
-    fn value(&self) -> &Self::V;
+    fn key(&'a self) -> Self::K;
+    fn value(&'a self) -> Self::V;
 }
 
-impl<K: Ord + Debug, V> KeyValue for (K, V) {
-    type K = K;
-    type V = V;
-    fn key(&self) -> &Self::K {
+impl<'a, KEY: Ord + Debug, VALUE> KeyValue<'a> for (KEY, VALUE)
+where
+    KEY: 'a,
+    VALUE: 'a,
+{
+    type K = &'a KEY;
+    type V = &'a VALUE;
+    fn key(&'a self) -> Self::K {
         &self.0
     }
-    fn value(&self) -> &Self::V {
+    fn value(&'a self) -> Self::V {
         &self.1
     }
 }
@@ -31,19 +35,19 @@ pub struct MapOfIndexes<T> {
     inner: Vec<T>,
 }
 
-impl<T: KeyValue> Default for MapOfIndexes<T> {
+impl<T: for<'a> KeyValue<'a>> Default for MapOfIndexes<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T: KeyValue> TryFrom<Vec<T>> for MapOfIndexes<T> {
+impl<T: for<'a> KeyValue<'a>> TryFrom<Vec<T>> for MapOfIndexes<T> {
     type Error = MapOfIndexesError;
 
     fn try_from(mut vec: Vec<T>) -> Result<Self, Self::Error> {
         // Other solution was to check duplicate while sorting, supposedly faster to make linear search after
         // when comparing elements is cheap
-        vec.sort_unstable_by(|a, b| a.key().cmp(b.key()));
+        vec.sort_unstable_by(|a, b| a.key().cmp(&b.key()));
         let duplicate = vec.windows(2).any(|w| w[0].key() == w[1].key());
         if duplicate {
             Err(MapOfIndexesError::DuplicateKeys)
@@ -53,7 +57,7 @@ impl<T: KeyValue> TryFrom<Vec<T>> for MapOfIndexes<T> {
     }
 }
 
-impl<T: KeyValue> MapOfIndexes<T> {
+impl<T: for<'a> KeyValue<'a>> MapOfIndexes<T> {
     pub fn new() -> Self {
         Self { inner: Vec::new() }
     }
@@ -78,14 +82,14 @@ impl<T: KeyValue> MapOfIndexes<T> {
         self.inner.push(element)
     }
 
-    fn get_idx(&self, key: &T::K) -> Option<usize> {
+    fn get_idx<'a>(&'a self, key: <T as KeyValue<'a>>::K) -> Option<usize> {
         if self.inner.len() == 0 {
             return None;
         }
         let mut idx = self.inner.len() / 2;
         for _ in 0..std::mem::size_of::<usize>() * 8 - self.inner.len().leading_zeros() as usize {
             // to handle 32bit targets
-            match self.inner[idx].key().cmp(key) {
+            match self.inner[idx].key().cmp(&key) {
                 Ordering::Less => idx = std::cmp::min(idx * 2, self.inner.len() - 1),
                 Ordering::Greater => idx /= 2,
                 Ordering::Equal => return Some(idx),
@@ -94,7 +98,7 @@ impl<T: KeyValue> MapOfIndexes<T> {
         None
     }
 
-    pub fn get(&self, key: &T::K) -> Option<&T::V> {
+    pub fn get<'a>(&'a self, key: <T as KeyValue<'a>>::K) -> Option<<T as KeyValue<'_>>::V> {
         self.get_idx(key).map(|idx| self.inner[idx].value())
     }
 
@@ -104,6 +108,35 @@ impl<T: KeyValue> MapOfIndexes<T> {
         }
     }
 }
+
+// TODO https://nora.codes/post/its-time-to-get-hyped-about-const-generics-in-rust/ statically check if mem_size<T> * 8 >= KEY_NB_BITS
+// pub struct CombinedKeyValue<T, const KEY_NB_BITS: usize, const VALUE_NB_BITS: usize>(T);
+
+// impl<T: From<usize>, const KEY_NB_BITS: usize, const VALUE_NB_BITS: usize> CombinedKeyValue<T, {KEY_NB_BITS}, {VALUE_NB_BITS}> {
+
+//     // To be run once after defining a type alias.
+//     // TODO use Macro instead(?)
+//     pub fn safety_check() {
+//         if std::mem::size_of::<T>() * 8 < KEY_NB_BITS + VALUE_NB_BITS {
+//             panic!("KEY_NB_BITS value is higher than the number of bits of the backup type.");
+//         }
+//     }
+
+//     pub fn new<K: Into<usize>, V: Into<usize>>(key: K, value: V) -> Self {
+//         Self(T::from(key.into() | value.into() << KEY_NB_BITS))
+//     }
+// }
+
+// impl<T: From<usize> + Ord + Debug + Copy, const KEY_NB_BITS: usize, const VALUE_NB_BITS: usize> KeyValue for  CombinedKeyValue<T, {KEY_NB_BITS}, {VALUE_NB_BITS}> where usize: From<T> {
+//     type K = T;
+//     type V = T;
+//     fn key(&self) -> Self::K {
+//         T::from(usize::from(self.0) & (usize::MAX >> VALUE_NB_BITS))
+//     }
+//     fn value(&self) -> Self::V {
+//         T::from(usize::from(self.0) >> KEY_NB_BITS)
+//     }
+// }
 
 #[cfg(test)]
 mod test {
